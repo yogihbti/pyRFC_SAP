3eb72c40f154b852378a1104e9e6d33986d27ec1
Yogesh kumar <yogihbti@users.noreply.github.com>
2019-06-12 14:22:01 +0530

    Config File Created

---
+++ con_config.py
@@ -0,0 +1,39 @@
+ECP = {
+    "user": "username",
+    "passwd": "password",
+    "ashost": "ip/hostname",
+    "sysnr": "XX",
+    "lang": "EN",
+    "client": "XXXX",
+    "sysid": "XXX"
+}
+
+
+SCP = {
+    "user": "username",
+    "passwd": "password",
+    "ashost": "ip/hostname",
+    "sysnr": "XX",
+    "lang": "EN",
+    "client": "XXXX",
+    "sysid": "XXX"
+}
+
+
+ECQ = {
+    "user": "username",
+    "passwd": "password",
+    "ashost": "ip/hostname",
+    "sysnr": "XX",
+    "lang": "EN",
+    "client": "XXXX",
+    "sysid": "XXX"
+}
+
+ctx_dict = {
+    "FROM_MONTH":"20180401",
+    "TO_MONTH":"20190331",
+    'FROM_YEAR':'2018',
+    'TO_YEAR':'2019'
+}
+
